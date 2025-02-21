be0d08a4ee7bfe785f214f8ed91eba48f861821e
Kyle <66798345+KingslayerKyle@users.noreply.github.com>
2025-02-21 20:11:49 +0000

    Add weights to new meshes
    Bug fix: Forgot to put the weights back on the new meshes

--- BO6Tools.py
+++ BO6Tools.py
@@ -23,6 +23,27 @@ def error( message ):
 def confirm_dialog( message ):
     cmds.confirmDialog( title = "Confirmation", message = message )
 
+def set_attribute( node, attribute, value ):
+    if cmds.objExists( node ):
+        if cmds.objExists( node + "." + attribute ):
+            cmds.setAttr( node + "." + attribute, value )
+
+def get_skinclusters():
+    return cmds.ls( type = "skinCluster" )
+
+def get_skincluster_for_mesh( mesh ):
+    return mel.eval( "findRelatedSkinCluster " + mesh )
+
+def get_selection():
+    return cmds.ls( selection = True )
+
+def set_skincluster_attributes():
+    for skinCluster in get_skinclusters():
+        set_attribute( skinCluster, "normalizeWeights", 1 )
+        set_attribute( skinCluster, "weightDistribution", 0 )
+        set_attribute( skinCluster, "maintainMaxInfluences", 1 )
+        set_attribute( skinCluster, "maxInfluences", 15 )
+
 def view_faces():
     mel.eval( "changeSelectMode -component" )
 
@@ -38,12 +59,22 @@ def delete_faces( faces_to_select ):
 
 def create_new_meshes_per_vertex_color( mesh, vertex_colors ):
     # Create a new mesh for each vertex color
-    for index, element in enumerate( vertex_colors ):
+    for color in vertex_colors:
         # Duplicate it
         duplicate = cmds.duplicate( mesh )[0]
 
+        # Clear selection
+        cmds.select( clear = True )
+
+        # Bind to joints from original mesh
+        cmds.select( cmds.skinCluster( get_skincluster_for_mesh( mesh ), query = True, influence = True ), add = True )
+        cmds.skinCluster( get_selection(), duplicate, toSelectedBones = True, maximumInfluences = 15, obeyMaxInfluences = True, dropoffRate = 5.0, removeUnusedInfluence = False, normalizeWeights = 1 )
+
+        # Transfer weights from original mesh
+        cmds.copySkinWeights( sourceSkin = get_skincluster_for_mesh( mesh ), destinationSkin = get_skincluster_for_mesh( duplicate ), noMirror = True, surfaceAssociation = "closestPoint", influenceAssociation = "oneToOne" )
+
         # Rename it
-        cmds.rename( duplicate, mesh + "_" + element )
+        cmds.rename( duplicate, mesh + "_" + color )
 
 def get_faces_for_vertex_color( mesh, index, inverse ):
     # Get all vertices of the mesh
@@ -94,18 +125,22 @@ def set_new_material_with_suffix( mesh, suffix ):
     cmds.sets( shape, edit = True, forceElement = new_shading_group )
 
 def split_mesh_by_vertex_colors():
-    if len( cmds.ls( selection = True ) ) < 1:
+    # Make sure something is selected
+    if len( get_selection() ) < 1:
         error( "Nothing is selected!" )
         return
-    
+
     # Get the selected mesh
-    selected_mesh = cmds.ls( selection = True )[0]
+    selected_mesh = get_selection()[0]
 
     # Vertex colors
     vertex_colors = ["red", "green", "blue"]
 
     # Create new meshes per vertex color
     create_new_meshes_per_vertex_color( selected_mesh, vertex_colors )
+
+    # Set skincluster attributes
+    set_skincluster_attributes()
 
     # Set a unique name for each shape
     for index, mesh in enumerate( cmds.ls( type = "mesh", long = True ) ):
@@ -146,7 +181,7 @@ def split_mesh_by_vertex_colors():
         # Get faces for vertex color
         faces_to_select = get_faces_for_vertex_color( duplicated_mesh, index, True )
 
-        # Inverse delete
+        # Delete faces
         delete_faces( faces_to_select )
 
         # Clear selection
@@ -171,11 +206,11 @@ def menu_items():
     # Create the menu
     main_menu = cmds.menu( "BO6Tools", label = "BO6Tools", tearOff = True )
 
-    # Misc
+    # Utilities
     cmds.menuItem( parent = main_menu, divider = True, dividerLabel = "Utilities" )
     cmds.menuItem( parent = main_menu, label = "Separate selected mesh", command = lambda x: split_mesh_by_vertex_colors() )
 
-    # Help
+    # Information
     cmds.menuItem( parent = main_menu, divider = True, dividerLabel = "Information" )
     cmds.menuItem( parent = main_menu, label = "About", command = lambda x: confirm_dialog( "=======\n BO6Tools\n=======\n\nRequirements:\n\nEnsure \"Export Vertex Colors\" is enabled in Greyhound.\n\nModels must be exported in the Cast model format:\nhttps://github.com/dtzxporter/cast/releases\n\nWhat does this plugin do?\n\nThis plugin separates a mesh into individual parts based on its vertex colors.\n\nIn newer CoD titles, vertex colors are used to designate image placements within a material.\n\nUnfortunately, this feature isn't supported in BO3, which is where this plugin comes in.\n\nThe process is fully automated, with new materials being created for each mesh per vertex color.\n\nOnce your mesh is separated, you can proceed with exporting your model as usual.\n\nby Kingslayer Kyle." ) )
 
